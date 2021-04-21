e1ee723101c61bb05695bf46e9275dc6f69be1c7
Kevin Mei <kevjinx@gmail.com>
2021-04-21 10:11:04 -0700

    finish

--- skeleton/recursive-debug.js
+++ skeleton/recursive-debug.js
@@ -13,9 +13,23 @@ doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
 doForAll(["a", "b", "c"]), (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
 
-function doForAll(arr, action) {
+function doForAll(arr, action = () => {}) {
+
+  debugger
+  if (!arr.length) {
+    debugger
+    return arr;
+  }
+
   return [action(arr[0]), ...doForAll(arr.slice(1), action)];
+
+
 }
+
+
+doForAll([], (x) => x * 2); // => []
+doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
+doForAll(["a", "b", "c"]), (x) => x.toUpperCase(); // => ["A", "B", "C"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
